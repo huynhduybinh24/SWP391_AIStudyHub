ed80a6014f076544e21441bcbc94b10d46e7f1d6
huynhduybinh24 <huynhduybinh242k5@gmail.com>
2026-05-21 20:33:16 +0700

    feat: initialize Zustand store for UI state management

--- FrontEnd/src/stores/uiStore.ts
+++ FrontEnd/src/stores/uiStore.ts
@@ -3,15 +3,23 @@ import { create } from 'zustand'
 interface UiState {
   sidebarOpen: boolean
   userMenuOpen: boolean
+  isChatPopupOpen: boolean
+  initialChatMessage: string
   setSidebarOpen: (open: boolean) => void
   toggleUserMenu: () => void
   setUserMenuOpen: (open: boolean) => void
+  setChatPopupOpen: (open: boolean) => void
+  setInitialChatMessage: (message: string) => void
 }
 
 export const useUiStore = create<UiState>((set) => ({
   sidebarOpen: true,
   userMenuOpen: false,
+  isChatPopupOpen: false,
+  initialChatMessage: '',
   setSidebarOpen: (sidebarOpen) => set({ sidebarOpen }),
   toggleUserMenu: () => set((s) => ({ userMenuOpen: !s.userMenuOpen })),
   setUserMenuOpen: (userMenuOpen) => set({ userMenuOpen }),
+  setChatPopupOpen: (isChatPopupOpen) => set({ isChatPopupOpen }),
+  setInitialChatMessage: (initialChatMessage) => set({ initialChatMessage }),
 }))
