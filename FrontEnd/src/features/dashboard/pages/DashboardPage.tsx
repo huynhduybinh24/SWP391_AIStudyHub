5db65d5965a92429b4f122e3d899b555d6973537
huynhduybinh24 <huynhduybinh242k5@gmail.com>
2026-05-23 17:49:01 +0700

    feat: add LandingPage, DashboardPage, HelpCenterPage, and study tracker service to the frontend architecture

--- FrontEnd/src/features/dashboard/pages/DashboardPage.tsx
+++ FrontEnd/src/features/dashboard/pages/DashboardPage.tsx
@@ -41,11 +41,7 @@ export function DashboardPage() {
       <div className="grid grid-cols-12 gap-6">
         <RecentDocuments documents={data.documents} />
         <QuickAskCard />
-        <WeeklyActivityChart
-          data={data.weeklyActivity}
-          totalHours={data.weeklyHours}
-          trend={data.weeklyTrend}
-        />
+        <WeeklyActivityChart />
         <RecentAlerts alerts={data.alerts} />
       </div>
 
