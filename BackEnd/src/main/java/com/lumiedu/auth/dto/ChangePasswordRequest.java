f3f706f4392d3a6f32f4920c209cc08d3a07da98
Vo Quoc Bao <baoachua642@gmail.com>
2026-06-03 22:02:06 +0700

    feat: implement authentication module with third-party account linking and password management DTOs

---
+++ BackEnd/src/main/java/com/lumiedu/auth/dto/ChangePasswordRequest.java
@@ -0,0 +1,15 @@
+package com.lumiedu.auth.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChangePasswordRequest {
+    private Long userId;
+    private String oldPassword;
+    private String newPassword;
+    private String confirmPassword;
+}
