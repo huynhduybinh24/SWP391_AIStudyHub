65fbc2e3796ace10b722db8f3fd894d198492351
Vo Quoc Bao <baoachua642@gmail.com>
2026-06-03 22:02:45 +0700

    feat: implement authentication service with registration, login, and password recovery functionality

---
+++ BackEnd/src/main/java/com/lumiedu/auth/dto/RegisterRequest.java
@@ -0,0 +1,15 @@
+package com.lumiedu.auth.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequest {
+    private String fullName;
+    private String email;
+    private String password;
+    private String confirmPassword;
+}
