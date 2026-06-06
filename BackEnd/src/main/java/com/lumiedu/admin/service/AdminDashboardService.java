a11ac9030b4b1c49fa56e5228611097997ec697b
LeQuocKhoi <khoigm2005@gmail.com>
2026-06-06 21:58:04 +0700

    Add admin service AdminDashboardService

---
+++ BackEnd/src/main/java/com/lumiedu/admin/service/AdminDashboardService.java
@@ -0,0 +1,7 @@
+package com.lumiedu.admin.service;
+
+import com.lumiedu.admin.dto.response.AdminDashboardStatsResponse;
+
+public interface AdminDashboardService {
+    AdminDashboardStatsResponse getStats();
+}
