3dcc86cc4879ee608ba4edaf13510b6fd6075565
vynk0912 <ngkhanhvy091204@gmail.com>
2026-06-06 09:51:36 +0700

    feat(notification): add NotificationRepository

---
+++ BackEnd/src/main/java/com/lumiedu/notification/repository/NotificationRepository.java
@@ -0,0 +1,21 @@
+package com.lumiedu.notification.repository;
+
+import com.lumiedu.notification.entity.Notification;
+import com.lumiedu.notification.enums.NotificationType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByUserIdAndDeletedFalseOrderByCreatedAtDesc(Long userId);
+
+    List<Notification> findByUserIdAndIsReadFalseAndDeletedFalseOrderByCreatedAtDesc(Long userId);
+
+    List<Notification> findByUserIdAndTypeInAndDeletedFalseOrderByCreatedAtDesc(Long userId, Collection<NotificationType> types);
+
+    long countByUserIdAndIsReadFalseAndDeletedFalse(Long userId);
+}
