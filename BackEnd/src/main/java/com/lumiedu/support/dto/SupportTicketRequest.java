b10f9870622c1324ed2e5a9cab607f1c3ce63d50
vynk0912 <ngkhanhvy091204@gmail.com>
2026-06-07 10:20:45 +0700

    feat: update SupportTicketRequest.java

---
+++ BackEnd/src/main/java/com/lumiedu/support/dto/SupportTicketRequest.java
@@ -0,0 +1,27 @@
+package com.lumiedu.support.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SupportTicketRequest {
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Subject cannot be blank")
+    private String subject;
+
+    @NotBlank(message = "Message cannot be blank")
+    private String message;
+}
